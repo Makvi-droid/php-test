ee0aec46b29bb4d9e533e5126561ec856f5e484a
Makvi-droid <pasbenauro05@gmail.com>
2024-11-14 16:56:48 +0800

    added files

--- variables-and-data_types/variables_dt.php
+++ variables-and-data_types/variables_dt.php
@@ -1,6 +1,13 @@
 <?php
 
 $name = "Ben Dover";
-echo $name;
+echo "Hello {$name}<br>";
+
+$currentYear = 2024;
+$born = 2003;
+
+$bday = $currentYear - $born;
+
+echo "You are currently {$bday} years old";
 
 ?>
