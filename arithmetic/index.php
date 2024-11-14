59564869ed13a8e7986ca96668320171484c2b86
Makvi-droid <pasbenauro05@gmail.com>
2024-11-14 17:25:43 +0800

    added new files for test

--- arithmetic/index.php
+++ arithmetic/index.php
@@ -1,3 +1,27 @@
 <?php
 
+$x = 10;
+$y = 20;
+$z = 35;
+$a = 2;
+$total = (4 + 50) * 2; 
+
+
+echo $x + $y;
+echo "<br>";
+
+echo $z - $x;
+echo "<br>";
+
+echo $z / $y;
+echo "<br>";
+
+echo $x * $y;
+echo "<br>";
+
+echo $y ** $a;
+echo "<br>";
+
+echo $total;
+
 ?>
