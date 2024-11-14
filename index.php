7d4e3897844450cfe4c9a39efad69626eaf32925
Makvi-droid <pasbenauro05@gmail.com>
2024-11-14 16:37:08 +0800

    added files

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+echo "PHP TEST <br>";
+echo "Trial success";
+//COMMENT TO ULOL
+/*multi
+    line
+    comment
+*/
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP TEST</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" 
+    rel="stylesheet" 
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" 
+    crossorigin="anonymous">
+
+</head>
+
+<body>
+
+    <h1>HTML</h1>
+    <button class="btn btn-primary">Click</button>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" 
+    crossorigin="anonymous"></script>
+
+</body>
+
+</html>
