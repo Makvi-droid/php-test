7f3046747c6080b5e2ecf30e578923d7917c2fd8
Makvi-droid <pasbenauro05@gmail.com>
2024-11-15 18:55:44 +0800

    added new files

---
+++ get&post/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Get and Post</title>
+</head>
+<body>
+    <label for="username">Username:</label><br>
+    <input type="text" name="username"><br>
+    <label for="password">Password:</label><br>
+    <input type="password" name="password"><br>
+    <button type="submit">Log in</button>
+</body>
+</html>
+
+<?php
+
+?>
